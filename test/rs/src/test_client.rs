b3a3c8232cea17a9147e56d621d53af9c0d1c063
Jeremy Fitzhardinge <jsgf@fb.com>
2016-08-10 11:59:31 +0200

    WIP tests

--- test/rs/src/test_client.rs
+++ test/rs/src/test_client.rs
@@ -24,6 +24,8 @@ extern crate getopts;
 use getopts::Options;
 use std::env;
 
+use std::collections::{BTreeMap, BTreeSet};
+
 use std::net::TcpStream;
 use bufstream::BufStream;
 use thrift::protocol::binary_protocol::BinaryProtocol;
@@ -31,12 +33,26 @@ use thrift::protocol::binary_protocol::BinaryProtocol;
 mod thrift_test;
 mod small_test;
 
-macro_rules! basetype_identity {
-    ( $client:expr, $func:ident, $val:expr ) => {
-        match $client.$func($val) {
-            Err(e) => panic!("failed on {} {:?}: {:?}", stringify!($func), $val, e),
-            Ok(Some(res)) if res == $val => println!("OK {} {:?}", stringify!($func), $val),
-            Ok(bad) => println!("failed on {} want {:?} got {:?}", stringify!($func), $val, bad),
+macro_rules! map {
+    () => { BTreeMap::new() };
+    ( $($key:expr => $val:expr ),* $(,)* ) => {
+        {
+            let mut map = BTreeMap::new();
+            $( map.insert($key.into(), $val.into()); )+
+            map
+        }
+    }
+}
+
+macro_rules! loopback {
+    ( $client:expr, $method:ident, $val:expr ) => {
+        let name = stringify!($method);
+        let val = $val;
+        print!("{}({:?}) => ", name, val);
+        match $client.$method(val.clone()) {
+            Ok(Some(ref v)) if v == &val => println!("{} OK", name),
+            Ok(bad) => panic!("{} failed bad {:?}", name, bad),
+            Err(err) => panic!("{} failed err {:?}", name, err),
         }
     }
 }
@@ -110,11 +126,7 @@ fn main() {
           Walon, Winaray, 吴语, isiXhosa, ייִדיש, Yorùbá, Zeêuws, 中文, 
           Bân-lâm-gú, 粵語";
 
-        match client.testString(s.into()) {
-            Err(e) => panic!("testString long failed {:?}", e),
-            Ok(Some(ref rs)) if rs == s => println!("testString long OK"),
-            Ok(bad) => panic!("testString long bad result {:?}", bad),
-        }
+        loopback!(client, testString, String::from(s));
     }
 
     {
@@ -126,57 +138,53 @@ fn main() {
            now-a-bunch-of-junk: !@#$%&()(&%$#{}{}<><><
            char-to-test-json-parsing: ]] \"]] \\\" }}}{ [[[ ";
 
-        match client.testString(s.into()) {
-            Err(e) => panic!("testString json failed {:?}", e),
-            Ok(Some(ref rs)) if rs == s => println!("testString json OK"),
-            Ok(bad) => panic!("testString json bad result {:?}", bad),
-        }
+        loopback! (client, testString, String::from(s));
     }
 
     // bool
-    basetype_identity!(client, testBool, true);
-    basetype_identity!(client, testBool, false);
+    loopback!(client, testBool, true);
+    loopback!(client, testBool, false);
 
     // i8
-    basetype_identity!(client, testByte, 0);
-    basetype_identity!(client, testByte, -1);
-    basetype_identity!(client, testByte, 42);
-    basetype_identity!(client, testByte, -42);
-    basetype_identity!(client, testByte, 127);
-    basetype_identity!(client, testByte, -128);
+    loopback!(client, testByte, 0);
+    loopback!(client, testByte, -1);
+    loopback!(client, testByte, 42);
+    loopback!(client, testByte, -42);
+    loopback!(client, testByte, 127);
+    loopback!(client, testByte, -128);
 
     // i32
-    basetype_identity!(client, testI32, 0);
-    basetype_identity!(client, testI32, -1);
-    basetype_identity!(client, testI32, 190000013);
-    basetype_identity!(client, testI32, -190000013);
-    basetype_identity!(client, testI32, std::i32::MAX);
-    basetype_identity!(client, testI32, std::i32::MIN);
+    loopback!(client, testI32, 0);
+    loopback!(client, testI32, -1);
+    loopback!(client, testI32, 190000013);
+    loopback!(client, testI32, -190000013);
+    loopback!(client, testI32, std::i32::MAX);
+    loopback!(client, testI32, std::i32::MIN);
 
     // i64
-    basetype_identity!(client, testI64, 0);
-    basetype_identity!(client, testI64, -1);
-    basetype_identity!(client, testI64, 7000000000000000123_i64);
-    basetype_identity!(client, testI64, -7000000000000000123_i64);
-    basetype_identity!(client, testI64, 2_i64.pow(32));
-    basetype_identity!(client, testI64, -2_i64.pow(32));
-    basetype_identity!(client, testI64, 2_i64.pow(32) + 1);
-    basetype_identity!(client, testI64, -2_i64.pow(32) - 1);
-    basetype_identity!(client, testI64, std::i64::MAX);
-    basetype_identity!(client, testI64, std::i64::MIN);
+    loopback!(client, testI64, 0);
+    loopback!(client, testI64, -1);
+    loopback!(client, testI64, 7000000000000000123_i64);
+    loopback!(client, testI64, -7000000000000000123_i64);
+    loopback!(client, testI64, 2_i64.pow(32));
+    loopback!(client, testI64, -2_i64.pow(32));
+    loopback!(client, testI64, 2_i64.pow(32) + 1);
+    loopback!(client, testI64, -2_i64.pow(32) - 1);
+    loopback!(client, testI64, std::i64::MAX);
+    loopback!(client, testI64, std::i64::MIN);
 
     // f64
     // Comparing double values with plain equality because Thrift handles full precision of double
-    basetype_identity!(client, testDouble, 0.0);
-    basetype_identity!(client, testDouble, -1.0);
-    basetype_identity!(client, testDouble, -5.2098523);
-    basetype_identity!(client, testDouble, -0.000341012439638598279);
-    basetype_identity!(client, testDouble, 2_f64.powi(32));
-    basetype_identity!(client, testDouble, 2_f64.powi(32) + 1_f64);
-    basetype_identity!(client, testDouble, 2_f64.powi(53) - 1_f64);
-    basetype_identity!(client, testDouble, -2_f64.powi(32));
-    basetype_identity!(client, testDouble, -2_f64.powi(32) - 1_f64);
-    basetype_identity!(client, testDouble, -2_f64.powi(53) + 1_f64);
+    loopback!(client, testDouble, 0.0);
+    loopback!(client, testDouble, -1.0);
+    loopback!(client, testDouble, -5.2098523);
+    loopback!(client, testDouble, -0.000341012439638598279);
+    loopback!(client, testDouble, 2_f64.powi(32));
+    loopback!(client, testDouble, 2_f64.powi(32) + 1_f64);
+    loopback!(client, testDouble, 2_f64.powi(53) - 1_f64);
+    loopback!(client, testDouble, -2_f64.powi(32));
+    loopback!(client, testDouble, -2_f64.powi(32) - 1_f64);
+    loopback!(client, testDouble, -2_f64.powi(53) + 1_f64);
 
     {
         let expected = 10_f64.powi(307);
@@ -224,10 +232,136 @@ fn main() {
            127
         ].iter().map(|v| *v as u8).collect();
         assert_eq!(data.len(), 256);
-        match client.testBinary(data.clone()) { // XXX fix ref params
-            Ok(Some(ref v)) if v == &data => println!("testBinary data OK"),
-            Ok(bad) => panic!("testBinary failed bad={:?}", bad),
-            Err(err) => panic!("testBinary failed err {:?}", err),
+        loopback!(client, testBinary, data);
+    }
+
+    {
+        let out = thrift_test::Xtruct {
+            string_thing: Some("Zero".into()),
+            byte_thing: Some(1),
+            i32_thing: Some(-3),
+            i64_thing: Some(-5),
+        };
+        loopback!(client, testStruct, out);
+    }
+
+    {
+        let out = thrift_test::Xtruct2 {
+            struct_thing: Some(thrift_test::Xtruct {
+                string_thing: Some("Zero".into()),
+                byte_thing: Some(1),
+                i32_thing: Some(-3),
+                i64_thing: Some(-5),
+            }),
+            byte_thing: Some(1),
+            i32_thing: Some(5),
+        };
+        loopback!(client, testNest, out);
+    }
+
+    {
+        let mut map = BTreeMap::new();
+        for i in 0..5 {
+            let _ = map.insert(i, i - 10);
         }
+        loopback!(client, testMap, map);
+    }
+
+    {
+        let map = map! { "b" => "b2", "a" => "abbbaababab", "blonkzonk" => "zooblezonk" };
+        loopback!(client, testStringMap, map);
+    }
+
+    {
+        let mut set = BTreeSet::new();
+        for i in -3..10 {
+            let _ = set.insert(i);
+        }
+        loopback!(client, testSet, set);
+    }
+
+    loopback!(client, testList, vec![]);
+    loopback!(client, testList, vec![-3,-2,1,4,5]);
+
+    {
+        use thrift_test::Numberz::*;
+        loopback!(client, testEnum, ONE);
+        loopback!(client, testEnum, TWO);
+        loopback!(client, testEnum, THREE);
+        loopback!(client, testEnum, FIVE);
+        loopback!(client, testEnum, SIX);
+        loopback!(client, testEnum, EIGHT);
+    }
+
+    loopback!(client, testTypedef, 309858235082523_i64);
+
+    match client.testMapMap(1) {
+        Ok(Some(v)) => {
+            // XXX TODO: check proper return
+            println!("testMapMap(1) returned {:?}", v);
+        },
+        Ok(bad) => panic!("testMapMap returned bad {:?}", bad),
+        Err(err) => panic!("testMapMap returned err {:?}", err),
+    }
+
+    {
+        use thrift_test::*;
+        use thrift_test::Numberz::*;
+
+        let insane = Insanity {
+            user_map: Some(map! { FIVE => 5, EIGHT => 8 }),
+            xtructs: Some(vec! [
+                Xtruct {
+                    string_thing: Some("Goodbye4".into()),
+                    byte_thing: Some(4),
+                    i32_thing: Some(4),
+                    i64_thing: Some(4),
+                },
+                Xtruct {
+                    string_thing: Some("Hello2".into()),
+                    byte_thing: Some(2),
+                    i32_thing: Some(2),
+                    i64_thing: Some(4),
+                }
+            ]),
+        };
+        match client.testInsanity(insane.clone()) {
+            Ok(Some(ref v)) => {
+                // XXX TODO: check proper return
+                println!("testInsanity({:?}) -> {:?}", insane, v)
+            },
+            Ok(bad) => panic!("testInsanity({:?}) returned bad {:?}", insane, bad),
+            Err(err) => panic!("testInsanity failed err {:?}", err),
+        }
+    }
+
+    {
+        use thrift_test::*;
+
+        match client.testMulti(42, 4242, 424242, map! { 1_i16 => "blah", 2_i16 => "thing" }, Numberz::EIGHT, 24) {
+            Ok(Some(ref res)) if res == &Xtruct { string_thing: Some("Hello2".into()), byte_thing: Some(42), i32_thing: Some(4242), i64_thing: Some(424242) } =>
+                println!("testMulti OK got res {:?}", res),
+            Ok(bad) => panic!("testMulti failed bad {:?}", bad),
+            Err(err) => panic!("testMulti failed err {:?}", err),
+        }
+    }
+
+    {
+        use thrift_test::*;
+        match client.testException("Xception".into()) {
+            Ok(Ok(bad)) => panic!("testException didn't except bad {:?}", bad),
+            Ok(Err(ThriftTestTestExceptionResult { success: None, err1: Some(ref exn) }))
+                if exn == &Xception { error_code: Some(1001), message: Some("Xception".into()) } =>
+                    println!("testException got {:?}", exn),
+            Ok(Err(bad)) => panic!("testException didn't except bad {:?}", bad),
+            Err(err) => panic!("testException got err {:?}", err),
+        }
+    }
+
+    // XXX FIXME void result returns None not ()
+    match client.testException("success".into()) {
+        Ok(Ok(())) => println!("testException(success) OK"),
+        Ok(bad) => panic!("testException(success) failed bad {:?}", bad),
+        Err(err) => panic!("testException(success) failed err {:?}", err),
     }
 }
